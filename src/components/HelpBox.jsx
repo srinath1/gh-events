9b2c9c00fdf0118f46d5a1c31856e20d850e01ec
srinath1 <srinath.erp@gmail.com>
2024-12-11 13:10:58 +0100

    m7

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -5,7 +5,7 @@ import "./HelpBox.css";
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h1>main branch_1</h1>
+      <h1>main branch_1_1</h1>
       <h3>{title}</h3>
       <p>{text}</p>
     </article>
